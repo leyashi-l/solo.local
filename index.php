df94139110993f48cd6439ca4320fab58102f722
leyashi <chiper322@gmail.com>
2024-10-23 21:11:32 +0300

    init

--- index.php
+++ index.php
@@ -9,61 +9,119 @@
 
 <body>
 <div class="wrapper">
-<header class="container">
-    <span class="logo">logo</span>
-    <nav>
-        <ul>
-            <li class="active"><a href="#">Home</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Portfolio</a></li>
-            <li><a href="#">News</a></li>
-            <li class="btn"><a href="#">Contacts</a></li>
-        </ul>
-    </nav>
-</header>
+    <header class="container">
+        <span class="logo">logo</span>
+        <nav>
+            <ul>
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">About us</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">News</a></li>
+                <li class="btn"><a href="#">Contacts</a></li>
+            </ul>
+        </nav>
+    </header>
 
-<div class="hero container">
-    <div class="heroinfo">
-        <h2>3D game Dev</h2>
-        <h1>Work that we produce for our clients</h1>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard</p>
-        <button class="btn">Get more details</button>
+    <div class="hero container">
+        <div class="heroinfo">
+            <h2>3D game Dev</h2>
+            <h1>Work that we produce for our clients</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+                industry`s standard</p>
+            <button class="btn">Get more details</button>
+        </div>
+        <img src="img/joystick.svg.svg" alt="">
     </div>
-    <img src="img/Group%201321.svg" alt="">
+    <div class="container trending">
+        <a href="#" class="see-all">SEE ALL</a>
+        <h3>Currently Trending Games</h3>
+
+        <div class="games">
+            <div class="block">
+                <img src="img/game%201.png" alt="">
+                <span><img src="img/fire.png" alt="">40 Followers</span>
+            </div>
+            <div class="block">
+                <img src="img/game%202.png" alt="">
+                <span><img src="img/fire.png" alt="">40 Followers</span>
+            </div>
+            <div class="block">
+                <img src="img/game%203.png" alt="">
+                <span><img src="img/fire.png" alt="">40 Followers</span>
+            </div>
+            <div class="block">
+                <img src="img/game%204.png" alt="">
+                <span><img src="img/fire.png" alt="">40 Followers</span>
+            </div>
+
+        </div>
+
+        <div class="container big-text">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+        </div>
+    </div>
+    <div class="container banner">
+        <h3>Lorem Ipsum</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s,</p>
+        <img src="img/banner.png" alt="">
+    </div>
 </div>
-<div class="container trending">
-    <a href="#" class="see-all">SEE ALL</a>
-    <h3>Currently Trending Games</h3>
 
-    <div class="games">
-        <div class="block">
-            <img src="img/Rectangle%2023.png" alt="">
-            <span><img src="img/fire%201.png" alt="">40 Followers</span>
+    <div class="features">
+        <div class="container">
+            <h3>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            <div class="info">
+                <div class="block">
+                    <img src="img/feature1.png" alt="">
+                    <p>Mobile Game Development</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+                <div class="block">
+                    <img src="img/feature2.png" alt="">
+                    <p>PC Game Development</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+                <div class="block">
+                    <img src="img/feature3.png" alt="">
+                    <p>PS4 Game Development</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+                <div class="block">
+                    <img src="img/feature4.png" alt="">
+                    <p>AR/VR Solutions</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+                <div class="block">
+                    <img src="img/feature5.png" alt="">
+                    <p>AR/ VR design</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+                <div class="block">
+                    <img src="img/feature6.png" alt="">
+                    <p>3D Modelings</p>
+                    <img src="img/arrow.png" alt="">
+                </div>
+            </div>
         </div>
-        <div class="block">
-            <img src="img/Rectangle%2024.png" alt="">
-            <span><img src="img/fire%201.png" alt="">40 Followers</span>
-        </div>
-        <div class="block">
-            <img src="img/Rectangle%2025.png" alt="">
-            <span><img src="img/fire%201.png" alt="">40 Followers</span>
-        </div>
-        <div class="block">
-            <img src="img/Rectangle%2026.png" alt="">
-            <span><img src="img/fire%201.png" alt="">40 Followers</span>
-        </div>
-
     </div>
 
-    <div class="container big-text">
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    <div class="wrapper">
+        <div class="container projects">
+            <h3>Our Recent Projects</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+            <div class="images">
+                <img src="img/project1.png" alt="">
+                <img src="img/project2.png" alt="">
+                <img src="img/project3.png" alt="">
+            </div>
+            <div class="images">
+                <img src="img/project4.png" alt="">
+                <img src="img/project5.png" alt="">
+                <img src="img/project6.png" alt="">
+        </div>
+            <a href="" class="see-all">SEE ALL</a>
     </div>
-</div>
-<div class="container banner">
-    <h3>Lorem Ipsum</h3>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
-    <img src="img/Rectangle%204.png" alt="">
-</div>
-</div>
 </body>
 </html>
