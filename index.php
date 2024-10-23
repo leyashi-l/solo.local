e208c4ef5fbead2aab3b28de6b3a70debb39887d
leyashi <chiper322@gmail.com>
2024-10-23 16:07:15 +0300

    init

---
+++ index.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stylish.css">
+    <title>Game Website</title>
+</head>
+
+<body>
+<div class="wrapper">
+<header class="container">
+    <span class="logo">logo</span>
+    <nav>
+        <ul>
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#">About us</a></li>
+            <li><a href="#">Portfolio</a></li>
+            <li><a href="#">News</a></li>
+            <li class="btn"><a href="#">Contacts</a></li>
+        </ul>
+    </nav>
+</header>
+
+<div class="hero container">
+    <div class="heroinfo">
+        <h2>3D game Dev</h2>
+        <h1>Work that we produce for our clients</h1>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry`s standard</p>
+        <button class="btn">Get more details</button>
+    </div>
+    <img src="img/Group%201321.svg" alt="">
+</div>
+<div class="container trending">
+    <a href="#" class="see-all">SEE ALL</a>
+    <h3>Currently Trending Games</h3>
+
+    <div class="games">
+        <div class="block">
+            <img src="img/Rectangle%2023.png" alt="">
+            <span><img src="img/fire%201.png" alt="">40 Followers</span>
+        </div>
+        <div class="block">
+            <img src="img/Rectangle%2024.png" alt="">
+            <span><img src="img/fire%201.png" alt="">40 Followers</span>
+        </div>
+        <div class="block">
+            <img src="img/Rectangle%2025.png" alt="">
+            <span><img src="img/fire%201.png" alt="">40 Followers</span>
+        </div>
+        <div class="block">
+            <img src="img/Rectangle%2026.png" alt="">
+            <span><img src="img/fire%201.png" alt="">40 Followers</span>
+        </div>
+
+    </div>
+
+    <div class="container big-text">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+    </div>
+</div>
+<div class="container banner"></div>
+</div>
+</body>
+</html>
